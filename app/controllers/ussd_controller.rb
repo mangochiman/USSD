7f2d1147c50570c81aff67a56621b8071369e42d
mangochiman <mangochiman@gmail.com>
2018-10-22 06:39:29 +0200

    USSD

--- app/controllers/ussd_controller.rb
+++ app/controllers/ussd_controller.rb
@@ -11,24 +11,34 @@ class UssdController < ApplicationController
     phone_number = params["phoneNumber"]
     text        = params["text"]
 
-    text = "" if text.split("*").include?("#")
+    if text.split("*").include?("#")
+      if text.split("*").last == "#"
+        text = ""
+      end
+      text = text.split("*").delete_if{|x|x== "#"}.join("*")
+    end
+    
     member = Member.find_by_phone_number(phone_number)
 
     if member.blank? ############ New members
       if (text == "" )
         response  = "CON Welcome #{phone_number}. Your phone number is not registered to Wella Funeral Services. Select action \n";
-        response += "1. Register \n";
-        response += "2. Check premiums \n";
-        response += "3. Exit \n";
+        response += "1. Register \n"
+        response += "2. Check premiums \n"
+        response += "3. Exit \n"
         
       elsif (text == "1" )
         response  = "CON Registration: \n Please enter your full name\n";
       elsif (text.match(/1*/i)) && (text.split("*").length == 2)
-        response  = "CON Please select gender: \n";
-        response += "1. Male \n";
-        response += "2. Female \n";
+        response  = "CON Please select gender: \n"
+        response += "1. Male \n"
+        response += "2. Female \n"
+      elsif (text.match(/1*/i)) && (text.split("*").length == 3) && [1,2].exclude?(text.split("*").last)
+        response  = "END Uknown option selected: Available options are \n"
+        response += "1. Male \n"
+        response += "2. Female \n"
       elsif (text.match(/1*/i)) && (text.split("*").length == 3)
-        response  = "CON District you are currently staying: \n";
+        response  = "CON District you are currently staying: \n"
       elsif (text.match(/1*/i)) && (text.split("*").length == 4)
         Member.enroll_in_program(params)
         response  = "CON We have successfully registered your phone number. Type # to go to main menu: \n";
@@ -39,38 +49,40 @@ class UssdController < ApplicationController
         response += "Press # to go to the main menu"
       elsif (text == "3" )
         #Exit
-        response = "END Sesssion terminated";
+        response = "END Sesssion terminated"
       else
-        response  = "CON Uknown option selected: \n";
-        response += "Press # to go to the main menu"
+        response  = "END Uknown option selected: Available options are \n"
+        response += "1. Register \n"
+        response += "2. Check premiums \n"
+        response += "3. Exit \n"
       end
     end
     
     unless member.blank?
       if (text == "" )
-        response  = "CON Welcome #{phone_number} to Wella Funeral Services. Select action \n";
-        response += "1. My account \n";
-        response += "2. Exit \n";
+        response  = "CON Welcome #{phone_number} to Wella Funeral Services. Select action \n"
+        response += "1. My account \n"
+        response += "2. Exit \n"
       elsif (text == "1" )
-        response  = "CON My account \n";
-        response += "1. Premiums \n";
-        response += "2. Dependants \n";
-        response += "3. Claims \n";
+        response  = "CON My account \n"
+        response += "1. Premiums \n"
+        response += "2. Dependants \n"
+        response += "3. Claims \n"
       elsif (text == "2" )
-        response  = "END session terminated \n";
+        response  = "END session terminated \n"
       elsif (text == "1*1" )
-        response  = "CON Premiums \n";
-        response += "1. Check balance \n";
-        response += "2. Pay premiums \n";
+        response  = "CON Premiums \n"
+        response += "1. Check balance \n"
+        response += "2. Pay premiums \n"
       elsif (text == "1*2" )
-        response  = "CON Dependants \n";
-        response += "1. Add dependant \n";
-        response += "2. Remove dependants \n";
-        response += "3. View dependants \n";
+        response  = "CON Dependants \n"
+        response += "1. Add dependant \n"
+        response += "2. Remove dependants \n"
+        response += "3. View dependants \n"
       elsif (text == "1*3" )
-        response  = "CON Claims \n";
-        response += "1. Make claim \n";
-        response += "2. My claims \n";
+        response  = "CON Claims \n"
+        response += "1. Make claim \n"
+        response += "2. My claims \n"
       end
     end
     
